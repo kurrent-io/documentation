8513ac6ca194ebc94d1f983ec8d41b8286b62ce8
Paulo Borges <paulohtb@hotmail.com>
2025-06-16 16:07:24 -0300

    engine: force dev-center redirects

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -44,6 +44,10 @@ export default defineUserConfig({
                 .replace("@clients/httpapi/", "/http-api/{version}/")
                 .replace("@httpapi/data/", projectionSamplesPath)
                 .replace("@httpapi/", "/http-api/{version}/")
+                // Add tutorial and use case redirects
+                .replace(/^\/tutorials\/(.*)/, "/dev-center/tutorials/$1")
+                .replace(/^\/getting-started\/use-cases\/(.*)\/tutorial-([1-5])\.(md|html)/, "/dev-center/use-cases/$1/tutorial/tutorial-$2.$3")
+                .replace(/^\/getting-started\/use-cases\/(.*)/, "/dev-center/use-cases/$1")
         });
         md.use(importCodePlugin, {
             handleImportPath: s => resolveMultiSamplesPath(s)
