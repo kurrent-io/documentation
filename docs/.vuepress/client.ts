f4563886654cab319d91becef6fb74e8e047d981
William Chong <william-chong@outlook.com>
2025-07-22 15:23:14 +0400

    Implement redirect for dotnet client only

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -102,11 +102,15 @@ export default defineClientConfig({
         addDynamicRoute("/server/kubernetes-operator", to => `/server/kubernetes-operator/${operatorLatest}/getting-started/`);
         addDynamicRoute("/server/kubernetes-operator/:version", to => `/server/kubernetes-operator/${to.params.version}/getting-started/`);
 
-        addDynamicRoute('/clients/grpc/:lang',
-            to => {
-              const latest = __VERSIONS__.all.filter(x => x.id == `${to.params.lang}-client`)[0].versions[0].version;
-              return `/clients/grpc/${to.params.lang}/${latest}/getting-started.html`
-            });
+        const dotnetClientLatest = __VERSIONS__.all.filter(x => x.id == 'dotnet-client')[0].versions[0].version;
+        addDynamicRoute("/clients/grpc/dotnet", to => `/clients/grpc/dotnet/${dotnetClientLatest}/getting-started.html`);
+
+        // // TODO: Uncomment until we finish writing the docs for the other clients
+        // addDynamicRoute('/clients/grpc/:lang',
+        //     to => {
+        //       const latest = __VERSIONS__.all.filter(x => x.id == `${to.params.lang}-client`)[0].versions[0].version;
+        //       return `/clients/grpc/${to.params.lang}/${latest}/getting-started.html`
+        //     });
 
         addDynamicRoute("/server/:version", to => `/server/${to.params.version}/quick-start/`);
         addDynamicRoute('/client/:lang',
