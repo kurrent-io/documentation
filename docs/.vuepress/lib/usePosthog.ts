94fcbafbc5ca0dc75fc8e239ec3afa67549bfa38
Stephen Tung <stktung@yahoo.com>
2025-11-27 13:48:26 +0800

    Revert "Analytics (#931)"
    This reverts commit e559b208627a80eb8291a7b1df5b4226029071b5.

--- docs/.vuepress/lib/usePosthog.ts
+++ docs/.vuepress/lib/usePosthog.ts
@@ -3,8 +3,7 @@ import posthog from 'posthog-js';
 export function usePostHog() {
     try {
         posthog.init('phc_DeHBgHGersY4LmDlADnPrsCPOAmMO7QFOH8f4DVEVmD', {
-            api_host: 'https://phog.kurrent.io',
-            capture_pageview: false
+            api_host: 'https://phog.kurrent.io'
         });
     } catch (e) {
     }
